8533767345bbb9f8a7d2ff7a357c535d2c1fa250
Uzair Aslam <uzairaslam990@gmail.com>
2023-12-19 08:19:17 -0800

    recentorder components changes

--- src/app/admin/(components)/Recentorder.jsx
+++ src/app/admin/(components)/Recentorder.jsx
@@ -3,6 +3,7 @@ import { data } from "../data/data.jsx";
 import { FaShoppingBag } from "react-icons/fa";
 const Recentorder = () => {
     return (
+      // Recentorder
         <div className='w-full col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-scroll dark:bg-gray-800 dark:text-white  no-scrollbar'>
           <h1>Recent Orders</h1>
           <ul>
